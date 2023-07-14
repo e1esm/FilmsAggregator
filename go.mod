b353f6195757776739f014c2a6b370bb88f3fd18
Egor Mikhailov <egor11022004@gmail.com>
2023-07-14 21:54:14 +0300

    Added ZapLogger to the project

--- go.mod
+++ go.mod
@@ -18,6 +18,9 @@ require (
 	github.com/restream/reindexer/v3 v3.17.0 // indirect
 	go.opentelemetry.io/otel v1.14.0 // indirect
 	go.opentelemetry.io/otel/trace v1.14.0 // indirect
+	go.uber.org/atomic v1.11.0 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
+	go.uber.org/zap v1.24.0 // indirect
 	golang.org/x/sys v0.0.0-20220114195835-da31bd327af9 // indirect
 	google.golang.org/protobuf v1.26.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
--- utils/logger/logger.go
+++ utils/logger/logger.go
@@ -1,1 +1,30 @@
 package logger
+
+import (
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+	"log"
+	"os"
+)
+
+var Logger zap.Logger
+
+func init() {
+	Logger = NewZapLogger()
+}
+
+func NewZapLogger() zap.Logger {
+	config := zap.NewDevelopmentEncoderConfig()
+	config.EncodeTime = zapcore.ISO8601TimeEncoder
+	fileEncoder := zapcore.NewJSONEncoder(config)
+
+	logFile, err := os.OpenFile("logs.json", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatalf("Couldn't have open/create logs.json file: %v", err)
+	}
+	writer := zapcore.AddSync(logFile)
+	defaultLogLevel := zapcore.DebugLevel
+	core := zapcore.NewTee(zapcore.NewCore(fileEncoder, writer, defaultLogLevel))
+
+	return *zap.New(core, zap.AddCaller(), zap.AddStacktrace(zapcore.ErrorLevel))
+}
