47d974df5b4a643166aa717d27b120d946105df8
Egor Mikhailov <egor11022004@gmail.com>
2023-07-15 21:06:24 +0300

    Basic functionality for the AddFilm func handler

--- internal/server/endpoints.go
+++ internal/server/endpoints.go
@@ -1,12 +1,38 @@
 package server
 
-import "net/http"
+import (
+	"encoding/json"
+	"github.com/e1esm/FilmsAggregator/internal/models"
+	"github.com/e1esm/FilmsAggregator/utils/logger"
+	"go.uber.org/zap"
+	"io"
+	"net/http"
+)
 
 func (s *AggregatorServer) AddFilm(w http.ResponseWriter, r *http.Request) {
+
 	if r.Method != http.MethodPost {
+		logger.Logger.Error("Another method was used on this URL",
+			zap.String("url", r.URL.String()))
 		w.WriteHeader(http.StatusMethodNotAllowed)
 		return
 	}
+	content, err := io.ReadAll(r.Body)
+	defer r.Body.Close()
+	if err != nil {
+		logger.Logger.Error("Couldn't have read the body of the request",
+			zap.String("err", err.Error()),
+			zap.String("url", r.URL.String()))
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	receivedFilm := &models.Film{}
+	err = json.Unmarshal(content, receivedFilm)
+	if err != nil {
+		logger.Logger.Error("Couldn't have unmarshalled request's body",
+			zap.String("err", err.Error()))
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("OK"))
 }
