099f0b74dc54a3c9b399b152dc33fd1613f96cd4
Egor Mikhailov <egor11022004@gmail.com>
2023-07-23 01:04:06 +0300

    Minor changes

--- internal/models/api/films.go
+++ internal/models/api/films.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"github.com/e1esm/FilmsAggregator/internal/models/db"
 	"github.com/e1esm/FilmsAggregator/internal/models/general"
 	"github.com/google/uuid"
 )
@@ -11,4 +12,13 @@ type Film struct {
 	Crew         general.Crew `json:"crew"`
 	ReleasedYear int          `json:"released_year" reindex:"released_year"`
 	Revenue      float64      `json:"revenue" reindex:"revenue"`
+}
+
+func NewFilm(film db.Film) *Film {
+	return &Film{ID: film.ID,
+		Title:        film.Title,
+		Crew:         film.Crew,
+		Revenue:      film.Revenue,
+		ReleasedYear: film.ReleasedYear,
+	}
 }
