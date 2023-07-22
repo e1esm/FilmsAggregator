1235ce0d2cd1dbc310b0409c6264a1e093f313a6
Egor Mikhailov <egor11022004@gmail.com>
2023-07-22 16:36:55 +0300

    Added tags

--- internal/models/films.go
+++ internal/models/films.go
@@ -6,24 +6,24 @@ import (
 )
 
 type Film struct {
-	ID           uuid.UUID `json:"-"`
-	Title        string    `json:"title"`
-	Crew         Crew      `json:"crew"`
-	ReleasedYear int       `json:"released_year"`
-	Revenue      float64   `json:"revenue"`
+	ID           uuid.UUID `json:"-" reindex:"id,,pk"`
+	Title        string    `json:"title" reindex:"title,tree"`
+	Crew         Crew      `json:"crew" reindex:"crew"`
+	ReleasedYear int       `json:"released_year" reindex:"released_year"`
+	Revenue      float64   `json:"revenue" reindex:"revenue"`
 	CacheTime    time.Time `json:"-" reindex:"cache_time"`
 }
 
 type Crew struct {
-	Actors    []Actor    `json:"actors"`
-	Producers []Producer `json:"producers"`
+	Actors    []Actor    `json:"actors" reindex:"actors"`
+	Producers []Producer `json:"producers" reindex:"producers"`
 }
 
 type Person struct {
-	ID        uuid.UUID `json:"-"`
-	Name      string    `json:"name"`
-	Birthdate string    `json:"birthdate"`
-	Gender    string    `json:"gender"`
+	ID        uuid.UUID `json:"-" reindex:"id,,pk"`
+	Name      string    `json:"name" reindex:"name"`
+	Birthdate string    `json:"birthdate" reindex:"birthdate"`
+	Gender    string    `json:"gender" reindex:"gender"`
 }
 
 func NewPerson(id uuid.UUID, name string, birthdate string, gender string) Person {
@@ -40,7 +40,7 @@ func NewProducer(id uuid.UUID, name string, birthdate string, gender string) *Pr
 
 type Actor struct {
 	Person
-	Role string `json:"role"`
+	Role string `json:"role" reindex:"role"`
 }
 
 func NewActor(id uuid.UUID, name string, birthdate string, gender string, role string) *Actor {
